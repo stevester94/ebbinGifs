fd844d684115323327523f1d86341ed1ff36cd0f
Steven Mackey <stevester94@gmail.com>
2014-07-05 14:27:40 -0700

    updated routes, gif_entries#index is now root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :gif_entry
+  root 'gif_entries#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
