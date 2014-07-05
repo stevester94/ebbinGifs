89334d933cc6f990f42cc5c8d0fb65c4f5198a36
Steven Mackey <stevester94@gmail.com>
2014-07-05 14:45:52 -0700

    added new and create methods, added new view

--- app/controllers/gif_entries_controller.rb
+++ app/controllers/gif_entries_controller.rb
@@ -3,4 +3,15 @@ class GifEntriesController < ApplicationController
 	def index
 	end
 
+	def new
+		@entry = GifEntry.new
+	end
+
+	def create
+		@entry = GifEntry.new(params.require(:gifentry).permit([:score, :url]))
+    @entry.save
+    redirect_to gifentry_path
+  end
+
+  
 end
