692e2bb8ae46a864d61915824f9c32df8cf4dbc0
Steven Mackey <stevester94@gmail.com>
2014-07-30 15:50:05 -0700

    added fields shortcount, cachedUps, cachedDowns to GifEntry.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140716202140) do
+ActiveRecord::Schema.define(version: 20140729175631) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,10 +25,10 @@ ActiveRecord::Schema.define(version: 20140716202140) do
   end
 
   create_table "favorites", force: true do |t|
+    t.integer  "user_id"
     t.string   "url"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
   end
 
   create_table "gif_entries", force: true do |t|
@@ -36,6 +36,9 @@ ActiveRecord::Schema.define(version: 20140716202140) do
     t.string   "url"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "shortCount"
+    t.text     "cachedUps"
+    t.text     "cachedDowns"
   end
 
   create_table "users", force: true do |t|
